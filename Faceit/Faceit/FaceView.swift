bd7f1c7ab50d4cd23e287b6f099fff1ab3bcfbff
championFu <qewradsf1028@gmail.com>
2017-06-08 14:59:22 +0800

    update

---
+++ Faceit/Faceit/FaceView.swift
@@ -0,0 +1,16 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by champion_fu on 06/06/.
+//  Copyright © 2017 champion_fu. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+}
---
+++ Faceit/Faceit/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by champion_fu on 06/06/.
+//  Copyright © 2017 champion_fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
